6686ef229beb8c6ea72907e3098bf402da535f8e
Janeq25 <janek2510@hotmail.com>
2025-11-16 18:29:17 +0100

    lab 5 done

---
+++ lab05/tb/tb.f
@@ -0,0 +1,14 @@
+simple_uart_switch_tb_pkg.sv
+simple_uart_switch_bfm.sv
+../../common/simple_uart_switch.svp
+top.sv
++incdir+tb_classes
++incdir+.
++nowarn+DSEMEL
+-uvm
+-uvmhome /eda/cadence/2024-25/RHELx86/XCELIUM_24.09.005/tools/methodology/UVM/CDNS-1.2/sv
++UVM_NO_RELNOTES
++UVM_VERBOSITY=MEDIUM
+-linedebug
+-fsmdebug
+-uvmlinedebug
---
+++ lab05/tb/tb_classes/random_tpgen.svh
@@ -0,0 +1,40 @@
+
+class random_tpgen extends base_tpgen;
+    `uvm_component_utils (random_tpgen)
+
+    function new (string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction : new
+
+
+
+    protected function byte get_data();
+    
+        bit [1:0] zero_ones;
+    
+        zero_ones = 2'($random);
+    
+        if (zero_ones == 2'b00)
+            return 8'h00;
+        else if (zero_ones == 2'b11)
+            return 8'hFF;
+        else
+            return 8'($random);
+    endfunction : get_data
+
+
+    protected function byte get_address();
+    
+        bit [1:0] zero_ones;
+    
+        zero_ones = 2'($random);
+    
+        if (zero_ones == 2'b00)
+            return 8'h00;
+        else if (zero_ones == 2'b11)
+            return 8'hFF;
+        else
+            return 8'($random);
+    endfunction : get_address
+
+endclass
